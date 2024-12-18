0f7dc7222aa56527b6fac10b9f6497ce1aec8a77
Daria Kuznetsova <ofgot17@gmail.com>
2024-12-18 21:26:36 +0100

    feat: delete build

--- build/CMakeFiles/Maze.dir/GameState.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Maze.dir/GameState.cpp.o: /Users/ofgot/Maze/GameState.cpp \
-  /Users/ofgot/Maze/GameState.h
--- build/CMakeFiles/Maze.dir/MenuState.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Maze.dir/MenuState.cpp.o: /Users/ofgot/Maze/MenuState.cpp \
-  /Users/ofgot/Maze/MenuState.h
