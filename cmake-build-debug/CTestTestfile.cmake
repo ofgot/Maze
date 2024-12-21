415be1492808b7767398f02643c0554328d9b94b
Daria Kuznetsova <ofgot17@gmail.com>
2024-12-21 22:02:30 +0100

    feat: final push

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ofgot/Maze
-# Build directory: /Users/ofgot/Maze/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[FieldTests]=] "/Users/ofgot/Maze/cmake-build-debug/FieldTests")
-set_tests_properties([=[FieldTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/ofgot/Maze/CMakeLists.txt;17;add_test;/Users/ofgot/Maze/CMakeLists.txt;0;")
